a746d557d54d36331d47f025b57da105f95778a8
Patrisha-de-Boon <32069658+Patrisha-de-Boon@users.noreply.github.com>
2017-12-06 20:13:42 -0700

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+void drawSplashScreen();
+void drawMenu();
+void win(uint16_t colour, uint8_t player);
+void drawScoreScreen(uint16_t p1Colour, uint16_t p2Colour, uint8_t bestOf);
+void drawScores(uint8_t score, uint8_t player);
+
+
+#endif
